b5b063e7cefbd81f2576141c542574630aaf93d0
pimphand <faisaldwiki69@gmail.com>
2024-12-13 19:52:52 +0700

    fixing

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const http = require("http");
 const app = require("./app");
 const port = 4000;
 
-const server = http.createServer(app);
+const startServer = () => {
+    const server = http.createServer(app);
 
-server.listen(port);
+    server.listen(port, () => {
+        console.log(`Server berjalan di port ${port}`);
+    });
+
+    server.on('error', (error) => {
+        console.error('Terjadi kesalahan:', error);
+        server.close(() => {
+            console.log('Server direstart');
+            startServer();  // Merestart server
+        });
+    });
+};
+
+startServer();
